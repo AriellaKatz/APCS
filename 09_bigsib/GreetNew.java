b4fa63008f124bb90796a9711b255e923f7c13a5
AriellaKatz <akatz30@stuy.edu>
2021-10-01 17:27:43 -0400

    Add cw/hw files

---
+++ 09_bigsib/GreetNew.java
@@ -0,0 +1,15 @@
+/*
+Ariella Katz
+APCS
+HW09 -- Referencing other classes
+This is a revised version of Greet.java from directory 08_hellofxn 
+  to be used in collaboration with BigSib.java in directory 09_bigsib.
+*/
+
+public class GreetNew{
+ public static void main(String[] args) {
+  BigSib.greet("Ho"); //As in Kaitlin Ho (no profanities here!)
+  BigSib.greet("Aleenis");
+  BigSib.greet("Tom");
+ }
+}
