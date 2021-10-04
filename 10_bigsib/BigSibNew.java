1777d4bdfee02ef6e7ad53649fe120b491dbc471
AriellaKatz <akatz30@stuy.edu>
2021-10-04 18:03:55 -0400

    adding to errors

---
+++ 10_bigsib/BigSibNew.java
@@ -0,0 +1,18 @@
+/*
+Ariella Katz
+APCS
+HW10 -- Different kinds of outputs
+worked with Tom, Jacob Kirmayer, Jacob Jr, Nafiz Labib, Martha
+*/
+
+/*
+DISCOVERIES:
+UNRESOLVED QUESTIONS:
+*/
+
+public class BigSibNew {
+ public static String greet(String name) {
+  String greeting = "What's up, " + name;
+  return greeting;
+ }
+}
---
+++ 10_bigsib/GreetNew.java
@@ -0,0 +1,15 @@
+/*
+Ariella Katz
+APCS
+HW10 -- Types of outputs
+This is a revised version of GreetNew.java from directory 09_bigsib 
+  to be used in collaboration with BigSibNew.java in directory 10_bigsib.
+*/
+
+public class GreetNew{
+ public static void main(String[] args) {
+  System.out.println(BigSibNew.greet("Crosby"));
+  System.out.println(BigSibNew.greet("Stills"));
+  System.out.println(BigSibNew.greet("Nash"));
+ }
+}
