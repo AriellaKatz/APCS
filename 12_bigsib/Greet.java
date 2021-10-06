afa8ad90f818338b879cc3acb666c58500f52953
AriellaKatz <akatz30@stuy.edu>
2021-10-05 21:11:30 -0400

    add hw12

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,20 @@
+/*
+Ariella Katz
+APCS
+HW12 -- Instance variables
+This is a revised version of Greet.java from directory 10_bigsib 
+  to be used in collaboration with BigSib.java in directory 12_bigsib.
+*/
+
+public class Greet{
+ public static void main(String[] args) {
+  String greeting;
+
+  BigSib richard = new BigSib();
+  richard.setHelloMsg("Word up");
+
+  greeting = richard.greet("freshman");
+  System.out.println(greeting);
+
+ }
+}
