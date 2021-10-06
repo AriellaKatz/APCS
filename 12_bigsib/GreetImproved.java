428727a86925e1868a6dfabe41c5d8ae6bcdc160
AriellaKatz <akatz30@stuy.edu>
2021-10-06 18:41:22 -0400

    improving last night's hw

--- 12_bigsib/GreetImproved.java
+++ 12_bigsib/GreetImproved.java
@@ -6,15 +6,36 @@ This is a revised version of Greet.java from directory 10_bigsib
   to be used in collaboration with BigSib.java in directory 12_bigsib.
 */
 
-public class Greet{
+public class GreetImproved{
  public static void main(String[] args) {
   String greeting;
+  String greeting1;
+  String greeting2;
+  String greeting3;
 
-  BigSib richard = new BigSib();
+  BigSibImproved richard = new BigSibImproved();
   richard.setHelloMsg("Word up");
 
   greeting = richard.greet("freshman");
   System.out.println(greeting);
+
+  BigSibImproved robert = new BigSibImproved();
+  robert.setHelloMsg("Salutations");
+
+  greeting1 = robert.greet("Dr. Spaceman");
+  System.out.println(greeting1);
+
+  BigSibImproved raymond = new BigSibImproved();
+  raymond.setHelloMsg("Hey ya");
+
+  greeting2 = raymond.greet("Kong Fooey");
+  System.out.println(greeting1);
+
+  BigSibImproved russel = new BigSibImproved();
+  russel.setHelloMsg("Sup");
+
+  greeting3 = russel.greet("mom");
+  System.out.println(greeting3);
 
  }
 }
