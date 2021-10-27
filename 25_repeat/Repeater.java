df14e5468e0ef4cf022f0b951579e008af05a11c
AriellaKatz <akatz30@stuy.edu>
2021-10-26 20:09:30 -0400

    hw25

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -1,3 +1,26 @@
+/*
+UnicornHead: Ariella Katz, Melody Lew, Lior Polischouk, Tom, Ollie, Toothless
+APCS
+HW25 -- Do I repeat myself?
+2021-10-26
+time spent: 1.0 hours
+*/
+
+/*
+DISCO:
+0. If you want to build off of the value of a variable in a loop of some sort (like with f
+   and oldF in fenceW()), you have to initialize it first; you can't just declare it.
+1. Returning the invocation of a method results in Java running the entire method, so if
+   the method prints anything in its body, this will also be among the outputs (not just
+   the value it returns). (See the else statement in fenceR().)
+QCC:
+0. You can't initialize variables in recursive loops (see DISCO 0) because the value will
+   reset every time. In fenceR, we sidestepped this by using System.out.print to concatenate
+   sections of the fence, but how else can we resolve this issue? Our way only works for
+   Strings - what if we want to alter the value of an integer with each recursive call? How
+   else can we create something to build off of?
+*/
+
 public class Repeater{
 
 	public static String fenceW ( int numPosts ) {
@@ -5,6 +28,9 @@ public class Repeater{
 		String f = "|";
 		String oldF = ""; //You must initialize it to be able to use it in the while loop below.
 
+		if (numPosts == 0) {
+			return ("No posts");
+		}
 		while (i <= numPosts) {
 			oldF = f;
 			f = f + "--|";
@@ -14,20 +40,20 @@ public class Repeater{
 	}
 
 	public static String fenceR ( int numPosts ) {
-		String f = ""; //How do I initialize f (so i can use it in the if else statement) in a way that won't reset it back to "" for each recursive call?
 		int n = numPosts;
 
+		if (n == 0) {
+			return ("No posts");
+		}
 		if (n == 1) {
-			String newF = f + "|";
+			return ("|");
 		}
 		else {
-			f = f + "|--";
+			System.out.print("|--"); //not println so it concatenates fence sections
 			n = n - 1;
-			fenceR(n);
+			return fenceR(n);
 		}
-		return f;
 	}
-
 	public static void main ( String[] args ) {
 		System.out.println(fenceW(4));
 		System.out.println(fenceR(4));
