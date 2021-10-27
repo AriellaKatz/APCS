f4102f3ffaa06d2cdc5355f99e454fbf444483fc
AriellaKatz <akatz30@stuy.edu>
2021-10-26 22:28:29 -0400

    lksjdf

---
+++ Extra/StaticMethods/OtherClass.java
@@ -0,0 +1,15 @@
+public class OtherClass{
+
+String instVar = "blah";
+
+//	public static String outsideStaticMethod () {
+//		instVar = "hello?";
+//		return instVar;
+//	}
+
+	public String outsideNonStaticMethod () {
+		instVar = "hi?";
+		return instVar;
+	}
+
+}
---
+++ Extra/StaticMethods/ThisClass.java
@@ -0,0 +1,10 @@
+public class ThisClass {
+
+public String instVar0;
+
+	public static void main (String[] args) {
+		OtherClass object = new OtherClass();
+		System.out.print(object.outsideNonStaticMethod());
+	}
+
+}
