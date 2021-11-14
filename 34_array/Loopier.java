e68cd0b409435654783a8c8fb8572e49cfb172be
AriellaKatz <akatz30@stuy.edu>
2021-11-14 13:31:49 -0500

    hw34

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -22,14 +22,55 @@ public class Loopier {
 	}
 
 	public static int linSearchR(int[] a, int target) {
-		int i
+		if (a.length == 0) { return -1;}
+		else {
+			if (a[0] == target) { return 0;}
+			else {
+				int[] aNew = new int[a.length-1];
+        		        for (int i = 1; i < a.length; i++) {
+                	        	aNew[i-1] = a[i];
+               			}
+				if ((a[1 + linSearchR(aNew, target)]) == target) {
+					return 1 + linSearchR(aNew, target); }
+				else { return -1; }
+			}
+		}
+	}
+
+	public static int freq(int[] a, int target) {
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == target) { count++; }
+		}
+		return count;
+	}
+
+	public static int freqRec(int[] a, int target) {
+                int[] aNew = new int[a.length-1];
+                for (int i = 1; i < a.length; i++) {
+                        aNew[i-1] = a[i];
+                }
+		if (a.length == 0) { return 0; }
+		else {
+			if (a[0] == target) {
+				if (aNew.length == 0) { return 1; }
+				else { return 1 + freqRec(aNew, target); }
+			}
+			else {
+				if (aNew.length == 0) { return 0; }
+				else { return freqRec(aNew, target); }
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		int[] array = new int[10];
+		int[] array = new int[25];
 		addInts(array);
 		System.out.println(stringArray(array));
 		System.out.println(linSearch(array,0));
+		System.out.println(linSearchR(array,0));
+		System.out.println(freq(array,0));
+		System.out.println(freqRec(array,0));
 	}
 
 }
