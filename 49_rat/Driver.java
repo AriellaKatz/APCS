d94975a01c1a94a557f381bfa970299ba42a3458
AriellaKatz <akatz30@stuy.edu>
2021-12-22 19:30:54 -0500

    49_rat

---
+++ 49_rat/Driver.java
@@ -0,0 +1,20 @@
+public class Driver {
+
+	public static void main(String[] args) {
+		Rational a = new Rational(1, 2);
+		Rational b = new Rational(3, 4);
+		Rational c = new Rational(3, 6);
+		String d = new String("5, 6");
+
+		System.out.println("1/2 = 3/4? " + a.equals(b));
+		System.out.println("1/2 vs 3/4: " + a.compareTo(b));
+		System.out.println("------------------------");
+		System.out.println("1/2 = 3/6? " + a.equals(c));
+		System.out.println("1/2 vs 3/6: " + a.compareTo(c));
+                System.out.println("------------------------");
+		System.out.println("1/2 = '5, 6'? " + a.equals(d));
+		System.out.println("1/2 vs '5, 6': " + a.compareTo(d));
+                System.out.println("------------------------");
+	}
+
+}
