3d65b7e89beb11bc5c67aeb7b9606a6486e09945
AriellaKatz <akatz30@stuy.edu>
2022-01-12 19:16:29 -0500

    activity2

--- cobo/magpie/activity2/Magpie2.java
+++ cobo/magpie/activity2/Magpie2.java
@@ -10,6 +10,25 @@
  * @author Laurie White
  * @version April 2012
  */
+
+ /*
+ * Keyword:	Response:
+ * homework	Homework is evil, how late do you stay up every night?
+ * track	Lettuce is the best!
+ * Erica	I heard that you are her fan.
+ *  When more than one keyword appears in a String, it gives the
+ * response that corresponds to the first, or leftmost, keyword.
+ * This is due to the use of else if statements to declare what the
+ * response should be. To prioritize a certain response, move it
+ * earlier in the chain of else if statements.
+ * Q1:
+ *   The problem with these statements is that the statements contain
+ *  keywords within other words. For example, "no" is in "know" and
+ *  "mother" is in "smothered". The indexOf() the respective keyword
+ *  would be >= 0 and therefore the response corresponding to that
+ *  keyword would be outputted.
+ */
+
 public class Magpie2
 {
 	/**
@@ -42,7 +61,7 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
-    else if (statement.indexOf("cat") >= 0
+    		else if (statement.indexOf("cat") >= 0
 		    || statement.indexOf("dog") >= 0
 		    || statement.indexOf("fish") >= 0)
 		{
@@ -54,7 +73,19 @@ public class Magpie2
 		}
 		else if ((statement.trim()).length() == 0)
 		{
-			response = "Say something,please.";
+			response = "Say something, please.";
+		}
+		else if ((statement.indexOf("homework") >= 0)
+		{
+			response = "Homework is evil, how late do you stay up every night?";
+		}
+		else if ((statement.indexOf("track") >= 0)
+		{
+			response = "Lettuce is the best!";
+		}
+		else if ((statement.indexOf("Erica") >= 0)
+		{
+			response = "I heard that you are her fan.";
 		}
 		else
 		{
