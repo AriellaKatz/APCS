b4c321296738592c76a9afb86dc2b4732ebff985
Ariella Katz <ariellakatz@Ariellas-MBP.cable.rcn.com>
2022-02-08 23:07:27 -0500

    hw61

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -1,8 +1,36 @@
+/*
+Team BAKK - Ariella Katz, Brian Kang, Tom, Vivian
+APCS
+HW61 - Instructions so Simple...
+2021-02-08
+time spent: 0.5 hrs
+*/
+
+/*
+DISCO:
+0. It would appear that when an array is passed through a method,
+   it is altered. But... see QCC0
+QCC:
+0. Why is it that in merge(), we had to do a0 = sort(a0) and
+   b0 = sort(b0) in order to proceed with sorted versions of a0
+   and b0? Just doing sort(a0) and sort(b0) was insufficient,
+   which would seem to contradict the hypothesis that passing a0
+   and b0 through sort() would actually reorder a0 and b0 instead of
+   returning sorted versions of them.
+1. How does the arrangement of the code or placement affect the
+   output when you run the code?
+*/
+
 /***
   class MergeSort
   Implements mergesort on array of ints.
   Summary of Algorithm:
-  ***/
+  0. Check if the array is already sorted (if its length is 1).
+  1. If it is, you're done.
+  2. If it isn't, divide it into two halves, sort those two halves
+     using the MergeSort algo (recursion!) and merge the now-sorted
+     halves.
+***/
 
 public class MergeSort
 {
@@ -15,8 +43,52 @@ public class MergeSort
    ******************************************************/
   private static int[] merge( int[] a, int[] b )
   {
-    int[] x = {0, 1};
-    return x;
+    int[] a0 = a;
+    a0 = sort(a0);
+    int[] b0 = b;
+    b0 = sort(b0);
+    int[] output = new int[a0.length + b0.length];
+    int i = 0;
+    while (a0.length > 0 && b0.length > 0) {
+      if (a0[0] < b0[0]) {
+        output[i] = a0[0];
+        //remove the first element from a0
+        int[] temp = new int[a0.length-1];
+        for (int m = 0; m < temp.length; m++) {
+          temp[m] = a0[m+1];
+        }
+        a0 = temp;
+      }
+      else {
+        output[i] = b0[0];
+        //remove the first element from b0
+        int[] temp = new int[b0.length-1];
+        for (int n = 0; n < temp.length; n++) {
+          temp[n] = b0[n+1];
+        }
+        b0 = temp;
+      }
+      i++;
+    }
+    //if a0 still has elements left over
+    if (a0.length > 0) {
+      //add the remaining elements in a0
+      for (int p = 0; p < a0.length; p++) {
+        //index of next element in output:
+        int index = output.length - a0.length + p;
+        output[index] = a0[p];
+      }
+    }
+    //otherwise, b0 will have elements left over
+    else {
+      //add the remaining elements in b0
+      for (int q = 0; q < b0.length; q++) {
+        //index of next element in output:
+        int index = output.length - b0.length + q;
+        output[index] = b0[q];
+      }
+    }
+    return output;
   }//end merge()
 
 
@@ -28,17 +100,25 @@ public class MergeSort
   public static int[] sort( int[] arr )
   {
     int[] output = new int[arr.length];
-    int x = arr.length/2;
-    int[] arrA = new int[x];
-    for (int m = 0; m < arrA.length; m++) {
-      arrA[m] = arr[m];
+    if (arr.length == 1) {
+      return arr;
     }
-    int[] arrB = new int[arr.length-x];
-    for (int n = 0; n < arrB.length; n++) {
-      arrB[n] = arr[x+n];
+    else {
+      int x = arr.length/2;
+      int[] arrA = new int[x];
+      int y = (arr.length - arr.length/2);
+      int[] arrB = new int[y];
+
+      for (int c = 0; c < arrA.length; c++) {
+        arrA[c] = arr[c];
+      }
+
+      for (int d = 0; d < arrB.length; d++) {
+        arrB[d] = arr[d + x];
+      }
+      output = merge(sort(arrA), sort(arrB));
+      return output;
     }
-    output = merge(arrA, arrB);
-    return output;
   }//end sort()
 
 
@@ -46,6 +126,8 @@ public class MergeSort
   //-------------------HELPERS-------------------------
   //tester function for exploring how arrays are passed
   //usage: print array, mess(array), print array. Whaddayasee?
+  // it appears that whatever happens to a in this method alters
+  // the array that is passed through it
   public static void mess( int[] a ) {
     for( int i = 0 ; i<a.length; i++ )
       a[i] = 0;
@@ -64,7 +146,7 @@ public class MergeSort
   //main method for testing
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
+
       int[] arr0 = {0};
       int[] arr1 = {1};
       int[] arr2 = {1,2};
@@ -86,6 +168,7 @@ public class MergeSort
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
+     /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
