643f9f2c0dc5f5cf3c529deb1a9f4c81e688edf7
AriellaKatz <akatz30@stuy.edu>
2022-02-14 12:34:34 -0500

    recursion-1

--- cb/recursion-1/Recursion1.java
+++ cb/recursion-1/Recursion1.java
@@ -45,6 +45,22 @@ fibonacci(2) → 1
 		else { return fibonacci(n-1) + (fibonacci(n-2)); }
 	}
 
+/*
+We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+bunnyEars2(0) → 0
+bunnyEars2(1) → 2
+bunnyEars2(2) → 5
+*/
+	public int bunnyEars2(int bunnies) {
+		if (bunnies == 0) { return 0; }
+		else if (bunnies == 1) { return 2; }
+		else {
+			if (bunnies % 2 == 0) { return (3 + bunnyEars2(bunnies-1)); }
+			else { return (2 + bunnyEars2(bunnies-1)); }
+		}
+	}
+
 	public static void main(String[] args) {
 		Recursion1 recurse = new Recursion1();
 		System.out.println("Expecting 1...");
@@ -106,8 +122,8 @@ fibonacci(2) → 1
 
                 System.out.println("-------------------------");
 
-		System.out.println("Expecting ...");
-		System.out.println(recurse.);
+		System.out.println("Expecting 0...");
+		System.out.println(recurse.bunnyEars2(0));
 	}
 
 }
