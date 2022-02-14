b0002ab8ee7aa653913a2b15b64373e9e46b0af0
AriellaKatz <akatz30@stuy.edu>
2022-02-14 12:27:30 -0500

    recurse-1

---
+++ cb/recursion-1/Recursion1.java
@@ -0,0 +1,113 @@
+public class Recursion1 {
+
+/*
+Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+
+factorial(1) → 1
+factorial(2) → 2
+factorial(3) → 6
+*/
+	public int factorial(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		else {
+			return (n * factorial(n-1));
+		}
+	}
+
+/*
+We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+bunnyEars(0) → 0
+bunnyEars(1) → 2
+bunnyEars(2) → 4
+*/
+	public int bunnyEars(int bunnies) {
+		if (bunnies == 0) {
+			return 0;
+		}
+		else {
+			return (2 + bunnyEars(bunnies-1));
+		}
+	}
+
+/*
+The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+
+fibonacci(0) → 0
+fibonacci(1) → 1
+fibonacci(2) → 1
+*/
+	public int fibonacci(int n) {
+		if (n == 0) { return 0; }
+		else if (n == 1) { return 1; }
+		else { return fibonacci(n-1) + (fibonacci(n-2)); }
+	}
+
+	public static void main(String[] args) {
+		Recursion1 recurse = new Recursion1();
+		System.out.println("Expecting 1...");
+		System.out.println(recurse.factorial(1));
+                System.out.println("Expecting 2...");
+                System.out.println(recurse.factorial(2));
+                System.out.println("Expecting 6...");
+                System.out.println(recurse.factorial(3));
+                System.out.println("Expecting 24...");
+                System.out.println(recurse.factorial(4));
+                System.out.println("Expecting 120...");
+                System.out.println(recurse.factorial(5));
+                System.out.println("Expecting 720...");
+                System.out.println(recurse.factorial(6));
+                System.out.println("Expecting 40320...");
+                System.out.println(recurse.factorial(8));
+                System.out.println("Expecting 479001600...");
+                System.out.println(recurse.factorial(12));
+
+		System.out.println("-------------------------");
+
+		System.out.println("Expecting 0...");
+		System.out.println(recurse.bunnyEars(0));
+                System.out.println("Expecting 2...");
+                System.out.println(recurse.bunnyEars(1));
+                System.out.println("Expecting 4...");
+                System.out.println(recurse.bunnyEars(2));
+                System.out.println("Expecting 6...");
+                System.out.println(recurse.bunnyEars(3));
+                System.out.println("Expecting 8...");
+                System.out.println(recurse.bunnyEars(4));
+                System.out.println("Expecting 10...");
+                System.out.println(recurse.bunnyEars(5));
+                System.out.println("Expecting 24...");
+                System.out.println(recurse.bunnyEars(12));
+                System.out.println("Expecting 100...");
+                System.out.println(recurse.bunnyEars(50));
+                System.out.println("Expecting 468...");
+                System.out.println(recurse.bunnyEars(234));
+
+                System.out.println("-------------------------");
+
+		System.out.println("Expecting 0...");
+		System.out.println(recurse.fibonacci(0));
+                System.out.println("Expecting 1...");
+                System.out.println(recurse.fibonacci(1));
+                System.out.println("Expecting 1...");
+                System.out.println(recurse.fibonacci(2));
+                System.out.println("Expecting 2...");
+                System.out.println(recurse.fibonacci(3));
+                System.out.println("Expecting 3...");
+                System.out.println(recurse.fibonacci(4));
+                System.out.println("Expecting 5...");
+                System.out.println(recurse.fibonacci(5));
+                System.out.println("Expecting 8...");
+                System.out.println(recurse.fibonacci(6));
+                System.out.println("Expecting 13...");
+                System.out.println(recurse.fibonacci(7));
+
+                System.out.println("-------------------------");
+
+		System.out.println("Expecting ...");
+		System.out.println(recurse.);
+	}
+
+}
