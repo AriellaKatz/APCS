5f6618c7caecf1b011166eb1db07334a0a027bc5
Ariella Katz <ariellakatz@Ariellas-MBP.cable.rcn.com>
2022-02-14 22:08:35 -0500

    hw64

--- cb/recursion-1/Recursion1.java
+++ cb/recursion-1/Recursion1.java
@@ -1,3 +1,11 @@
+/*
+Team AK: Ariella Katz, Kaitlin Ho, Tom, Apple
+APCS
+HW64: Revisitation
+2022-02-14
+time spent: 0.5 hrs
+*/
+
 public class Recursion1 {
 
 /*
@@ -61,69 +69,112 @@ bunnyEars2(2) → 5
 		}
 	}
 
+/*
+We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+triangle(0) → 0
+triangle(1) → 1
+triangle(2) → 3
+*/
+	public int triangle(int rows) {
+		if (rows == 0) { return 0; }
+		else { return (rows + triangle(rows-1)); }
+	}
+
 	public static void main(String[] args) {
 		Recursion1 recurse = new Recursion1();
 		System.out.println("Expecting 1...");
 		System.out.println(recurse.factorial(1));
-                System.out.println("Expecting 2...");
-                System.out.println(recurse.factorial(2));
-                System.out.println("Expecting 6...");
-                System.out.println(recurse.factorial(3));
-                System.out.println("Expecting 24...");
-                System.out.println(recurse.factorial(4));
-                System.out.println("Expecting 120...");
-                System.out.println(recurse.factorial(5));
-                System.out.println("Expecting 720...");
-                System.out.println(recurse.factorial(6));
-                System.out.println("Expecting 40320...");
-                System.out.println(recurse.factorial(8));
-                System.out.println("Expecting 479001600...");
-                System.out.println(recurse.factorial(12));
+    System.out.println("Expecting 2...");
+    System.out.println(recurse.factorial(2));
+    System.out.println("Expecting 6...");
+    System.out.println(recurse.factorial(3));
+    System.out.println("Expecting 24...");
+    System.out.println(recurse.factorial(4));
+    System.out.println("Expecting 120...");
+    System.out.println(recurse.factorial(5));
+    System.out.println("Expecting 720...");
+    System.out.println(recurse.factorial(6));
+    System.out.println("Expecting 40320...");
+    System.out.println(recurse.factorial(8));
+    System.out.println("Expecting 479001600...");
+    System.out.println(recurse.factorial(12));
 
 		System.out.println("-------------------------");
 
 		System.out.println("Expecting 0...");
 		System.out.println(recurse.bunnyEars(0));
-                System.out.println("Expecting 2...");
-                System.out.println(recurse.bunnyEars(1));
-                System.out.println("Expecting 4...");
-                System.out.println(recurse.bunnyEars(2));
-                System.out.println("Expecting 6...");
-                System.out.println(recurse.bunnyEars(3));
-                System.out.println("Expecting 8...");
-                System.out.println(recurse.bunnyEars(4));
-                System.out.println("Expecting 10...");
-                System.out.println(recurse.bunnyEars(5));
-                System.out.println("Expecting 24...");
-                System.out.println(recurse.bunnyEars(12));
-                System.out.println("Expecting 100...");
-                System.out.println(recurse.bunnyEars(50));
-                System.out.println("Expecting 468...");
-                System.out.println(recurse.bunnyEars(234));
+    System.out.println("Expecting 2...");
+    System.out.println(recurse.bunnyEars(1));
+    System.out.println("Expecting 4...");
+    System.out.println(recurse.bunnyEars(2));
+    System.out.println("Expecting 6...");
+    System.out.println(recurse.bunnyEars(3));
+    System.out.println("Expecting 8...");
+    System.out.println(recurse.bunnyEars(4));
+    System.out.println("Expecting 10...");
+    System.out.println(recurse.bunnyEars(5));
+    System.out.println("Expecting 24...");
+    System.out.println(recurse.bunnyEars(12));
+    System.out.println("Expecting 100...");
+    System.out.println(recurse.bunnyEars(50));
+    System.out.println("Expecting 468...");
+    System.out.println(recurse.bunnyEars(234));
 
-                System.out.println("-------------------------");
+    System.out.println("-------------------------");
 
 		System.out.println("Expecting 0...");
 		System.out.println(recurse.fibonacci(0));
-                System.out.println("Expecting 1...");
-                System.out.println(recurse.fibonacci(1));
-                System.out.println("Expecting 1...");
-                System.out.println(recurse.fibonacci(2));
-                System.out.println("Expecting 2...");
-                System.out.println(recurse.fibonacci(3));
-                System.out.println("Expecting 3...");
-                System.out.println(recurse.fibonacci(4));
-                System.out.println("Expecting 5...");
-                System.out.println(recurse.fibonacci(5));
-                System.out.println("Expecting 8...");
-                System.out.println(recurse.fibonacci(6));
-                System.out.println("Expecting 13...");
-                System.out.println(recurse.fibonacci(7));
+    System.out.println("Expecting 1...");
+    System.out.println(recurse.fibonacci(1));
+    System.out.println("Expecting 1...");
+    System.out.println(recurse.fibonacci(2));
+    System.out.println("Expecting 2...");
+    System.out.println(recurse.fibonacci(3));
+    System.out.println("Expecting 3...");
+    System.out.println(recurse.fibonacci(4));
+    System.out.println("Expecting 5...");
+  	System.out.println(recurse.fibonacci(5));
+    System.out.println("Expecting 8...");
+    System.out.println(recurse.fibonacci(6));
+    System.out.println("Expecting 13...");
+    System.out.println(recurse.fibonacci(7));
 
-                System.out.println("-------------------------");
+    System.out.println("-------------------------");
 
 		System.out.println("Expecting 0...");
 		System.out.println(recurse.bunnyEars2(0));
+		System.out.println("Expecting 2...");
+		System.out.println(recurse.bunnyEars2(1));
+		System.out.println("Expecting 5...");
+		System.out.println(recurse.bunnyEars2(2));
+		System.out.println("Expecting 7...");
+		System.out.println(recurse.bunnyEars2(3));
+		System.out.println("Expecting 10...");
+		System.out.println(recurse.bunnyEars2(4));
+		System.out.println("Expecting 15...");
+		System.out.println(recurse.bunnyEars2(6));
+		System.out.println("Expecting 25...");
+		System.out.println(recurse.bunnyEars2(10));
+
+    System.out.println("-------------------------");
+
+		System.out.println("Expecting 0...");
+		System.out.println(recurse.triangle(0));
+		System.out.println("Expecting 1...");
+		System.out.println(recurse.triangle(1));
+		System.out.println("Expecting 3...");
+		System.out.println(recurse.triangle(2));
+		System.out.println("Expecting 6...");
+		System.out.println(recurse.triangle(3));
+		System.out.println("Expecting 10...");
+		System.out.println(recurse.triangle(4));
+		System.out.println("Expecting 15...");
+		System.out.println(recurse.triangle(5));
+		System.out.println("Expecting 21...");
+		System.out.println(recurse.triangle(6));
+		System.out.println("Expecting 28...");
+		System.out.println(recurse.triangle(7));
 	}
 
 }
