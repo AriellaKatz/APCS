c6d2625966c3436c2692051eb65eca676f5b1c69
Ariella Katz <ariellakatz@Ariellas-MBP.cable.rcn.com>
2022-03-14 21:19:06 -0400

    hw76

---
+++ 76_llist/LLNode.java
@@ -0,0 +1,118 @@
+/*
+Team Pink Lemonade (Ariella Katz, Jacob Ng, Emily Ortiz, Tom, Preguac, Applesauce)
+APCS pd6
+HW75: Nodal Recall
+2022-03-11
+time spent: 0.75 hours
+*/
+
+/***
+ * class LLNode
+ * Implements a node, for use in lists and other container classes.
+ * Stores its data as a String
+ **/
+
+public class LLNode
+{
+  //instance vars
+  private String _cargo;
+  private LLNode _nextNode;
+
+  // constructor
+  public LLNode( String value, LLNode next )
+  {
+    _cargo = value;
+    _nextNode = next;
+  }
+
+
+  //--------------v  ACCESSORS  v--------------
+  public String getCargo()
+  {
+    return _cargo;
+  }
+
+  public LLNode getNext()
+  {
+    return _nextNode;
+  }
+  //--------------^  ACCESSORS  ^--------------
+
+
+  //--------------v  MUTATORS  v--------------
+  public String setCargo( String newCargo )
+  {
+    String oldCargo = getCargo();
+    _cargo = newCargo;
+    return oldCargo;
+  }
+
+  public LLNode setNext( LLNode newNext )
+  {
+    LLNode oldNext = getNext();
+    _nextNode = newNext;
+    return oldNext;
+  }
+  //--------------^  MUTATORS  ^--------------
+
+
+  // override inherited toString
+  public String toString()
+  {
+    String retStr = "";
+    LLNode temp = this;
+    while (temp != null) {
+      retStr += temp.getCargo() + ", ";
+      temp = temp.getNext();
+    }
+    retStr = retStr.substring(0,retStr.length()-2);
+    return retStr;
+  }
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+    //Below is an exercise in creating a linked list...
+
+    //Create a node
+    LLNode first = new LLNode( "cat", null );
+    System.out.println(first);
+
+    //Create a new node after the first
+    first.setNext( new LLNode( "dog", null ) );
+    System.out.println(first);
+
+    //Create a third node after the second
+    first.getNext().setNext( new LLNode( "cow", null ) );
+    System.out.println(first);
+
+    // A naive list traversal, has side effects.... ??
+    /*
+       while( first != null ) {
+       System.out.println( first );
+       first = first.getNext();
+       }
+       System.out.println(first);
+    */
+
+    //Q: when head ptr moves to next node in list, what happens to the node it
+    //   just left?
+    //A: first is no longer assigned to the original node, so it's effectively
+    //   deleted because there is no longer anything pointing to it
+    //...so better: ?
+
+        LLNode temp = first;
+        //this way, first isn't actually changed during the traversal (only temp
+        //is changed)
+        while (temp != null) {
+          System.out.println(temp);
+          temp = temp.getNext();
+        }
+        System.out.println(first);
+        //System.out.println(temp);
+
+  }//end main
+
+}//end class LLNode
--- 76_llist/LList.java
+++ 76_llist/LList.java
@@ -1,3 +1,16 @@
+/*
+Team Pink Lemonade (Ariella Katz, Jacob Ng, Emily Ortiz, Tom, Preguac, Applesauce)
+APCS pd6
+HW76: We Got a Little Ol' Convoy
+2022-03-14
+time spent: 1 hours
+*/
+
+/*
+QCC
+* Is the tail end supposed to be 0 index?
+* What do we do with head node?
+*?
 /***
  * class LList
  * Implements a linked list of LLNodes, each containing String data
@@ -14,6 +27,8 @@ public class LList implements List //interface def must be in this dir
   public LList( )
   {
     // YOUR CODE HERE
+    _head = new LLNode ("", null);           //what do we do for head? Is an empty node possible?
+    _size = 0;
   }
 
 
@@ -22,6 +37,14 @@ public class LList implements List //interface def must be in this dir
   public boolean add( String newVal )
   {
     // YOUR CODE HERE
+    if (_size == 0) { _head.setCargo(newVal); }
+    LLNode temp = _head;
+    while( temp.getNext() != null ) {       //use .getnext so that temp equals the last node not null
+    temp = temp.getNext();
+    }
+    temp.setNext(new LLNode(newVal, null));
+    _size ++;
+    return true;
   }
 
 
@@ -31,6 +54,12 @@ public class LList implements List //interface def must be in this dir
       throw new IndexOutOfBoundsException();
 
     // YOUR CODE HERE
+    LLNode temp = _head;
+    for(int i = 0; i <= index; i++){
+      temp = temp.getNext();
+      //System.out.println(temp);
+    }
+      return temp.getCargo();
   }
 
 
@@ -41,6 +70,11 @@ public class LList implements List //interface def must be in this dir
       throw new IndexOutOfBoundsException();
 
     // YOUR CODE HERE
+    LLNode temp = _head;
+    for(int i = 0; i < _size - index - 1; i++){
+      temp = temp.getNext();
+    }
+      return temp.setCargo(newVal);
   }
 
 
@@ -48,6 +82,7 @@ public class LList implements List //interface def must be in this dir
   public int size()
   {
     // YOUR CODE HERE
+    return _size;
   }
 
   //--------------^  List interface methods  ^--------------
@@ -58,40 +93,45 @@ public class LList implements List //interface def must be in this dir
   public String toString()
   {
     // YOUR CODE HERE
-  }
+    String retStr = "";
+    //LLNode temp = _head;
+    for(int i = 0; i < _size; i++){
+      retStr += get(i) + ", ";
+      //temp = temp.getNext();
+    }
+    if (_size > 0) {
+      retStr = retStr.substring(0,retStr.length()-2);
+    }
+    return retStr;
 
+  }
 
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     LList james = new LList();
-
     System.out.println( james );
-    System.out.println( "size: " + james.size() );
 
+    System.out.println( "size: " + james.size() );
     james.add("beat");
     System.out.println( james );
     System.out.println( "size: " + james.size() );
-
     james.add("a");
     System.out.println( james );
     System.out.println( "size: " + james.size() );
-
     james.add("need");
     System.out.println( james );
     System.out.println( "size: " + james.size() );
-
     james.add("I");
     System.out.println( james );
     System.out.println( "size: " + james.size() );
-
     System.out.println( "2nd item is: " + james.get(1) );
-
     james.set( 1, "got" );
     System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
-
     System.out.println( james );
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }
 
