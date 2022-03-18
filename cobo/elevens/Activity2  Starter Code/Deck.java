c9e5c9d3b49e90fff3b32481a4561147666f3fbd
AriellaKatz <akatz30@stuy.edu>
2022-03-18 12:30:32 -0400

    activity2 start

--- cobo/elevens/Activity2  Starter Code/Deck.java
+++ cobo/elevens/Activity2  Starter Code/Deck.java
@@ -30,7 +30,12 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		cards = new List<Card>();
+		for (int i = 0; i < ranks.length; i++) {
+			cards.add(new Card(ranks[i], suits[i], values[i]));
+		}
+		size = cards.length;
+		cards.shuffle();
 	}
 
 
@@ -39,7 +44,7 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return (size == 0);
 	}
 
 	/**
@@ -47,7 +52,7 @@ public class Deck {
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return size;
 	}
 
 	/**
@@ -64,7 +69,8 @@ public class Deck {
 	 *         previously dealt.
 	 */
 	public Card deal() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		size--;
+		return cards[size];
 	}
 
 	/**
