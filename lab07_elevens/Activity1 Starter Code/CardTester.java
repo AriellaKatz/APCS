048e3b7cc2385e5a4d3c7dfdaf06b678276abcd7
AriellaKatz <akatz30@stuy.edu>
2022-03-31 19:31:39 -0400

    hw86

---
+++ lab07_elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,34 @@
+/*
+Team Pink Lemonade: Ariella Katz, Emily Ortiz, Jacob Ng
+APCS pd6
+L07: But These Go Up To Eleven
+2022-03-18
+time spent: 0.25 hrs
+*/
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card card0 = new Card("two", "spades", 2);
+		Card card1 = new Card("two", "spades", 2);
+		Card card2 = new Card("two", "hearts", 2);
+
+		System.out.println("Card0: " + card0);
+		System.out.println("Card1: " + card1);
+		System.out.println("Card2: " + card2);
+
+		System.out.println("Card0 = Card1?");
+		System.out.println(card0.matches(card1));
+		System.out.println("Card0 = Card2?");
+		System.out.println(card0.matches(card2));
+		System.out.println("Card1 = Card2?");
+		System.out.println(card1.matches(card2));
+	}
+}
