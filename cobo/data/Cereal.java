27012643d0d68a663a5bb44fe1822addb70d385a
Ariella Katz <ariellakatz@Ariellas-MBP.cable.rcn.com>
2022-04-05 19:33:28 -0400

    data

---
+++ cobo/data/Cereal.java
@@ -0,0 +1,13 @@
+/*
+The Greyducks :: Kaitlin Ho, Hugo Jenkins, Ariella Katz, Apple, Boary, Tom
+APCS
+LAB08 -- America's Next Top Data Scientist
+2022-04-05
+time spent: 1.0 hrs
+*/
+
+public class Cereal {
+
+  
+
+}
